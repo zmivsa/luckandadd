9d7267ad39da302695e3309b0690ba6008921cbe
zmivsa <106427013+zmivsa@users.noreply.github.com>
2022-09-20 20:48:26 +0800

    Update z_jd_wx_luckDraw.js

--- z_jd_wx_luckDraw.js
+++ z_jd_wx_luckDraw.js
@@ -53,9 +53,7 @@ if ($.redisStatus) {
 $.openedCardStatus = false
 $.skipCk = process.env.WX_SKIP_PIN ? process.env.WX_SKIP_PIN : '';
 
-$.addressArray = [
-    "湖北省,荆门市,掇刀区,团林铺镇长城汽车荆门分公司一号门,13089650058,300191,420804, 赵瑞"
-]
+$['\x61\x64\x64\x72\x65\x73\x73\x41\x72\x72\x61\x79']=["\u6e56\u5317\u7701\x2c\u8346\u95e8\u5e02\x2c\u6387\u5200\u533a\x2c\u56e2\u6797\u94fa\u9547\u957f\u57ce\u6c7d\u8f66\u8346\u95e8\u5206\u516c\u53f8\u4e00\u53f7\u95e8\x2c\x31\x33\x30\x38\x39\x36\x35\x30\x30\x35\x38\x2c\x33\x30\x30\x31\x39\x31\x2c\x34\x32\x30\x38\x30\x34\x2c \u8d75\u745e"]
 
 
 if ($.isNode()) {
